745b89b9fc9631364d85ccb26b7c84bf8fe35f04
Alex Vazquez <alex.vazquez708@gmail.com>
2019-04-11 14:59:59 -0400

    The link to download spark changed because the version changed

--- install_spark.sh
+++ install_spark.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-wget https://www-eu.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+wget https://www-eu.apache.org/dist/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
 tar -xzf spark-2.4.0-bin-hadoop2.7.tgz -C /opt/
 cp /local/repository/spark-env.sh /opt/spark-2.4.0-bin-hadoop2.7/conf/spark-env.sh
